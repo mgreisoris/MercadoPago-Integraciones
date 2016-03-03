1c875842cec282e9869b0f3bc78a8201969aad0b
slimardi <stefania.limardi@mercadolibre.com>
2016-03-03 10:17:23 -0300

    Added files via upload

---
+++ create_preference.php
@@ -0,0 +1,46 @@
+<?php
+
+ini_set('display_errors',1);  error_reporting(E_ALL);
+require_once "../lib/mercadopago.php";
+ 
+ 
+
+$mp = new MP('CLIENT_ID', 'CLIENT_SECRET'); //poner credenciales del mayorista
+ 
+$preference_data = array (
+    "items" => array(
+        array(
+            "title" => "Reserva de turismo",
+            "quantity" => 1,
+            "currency_id" => "ARS",
+            "unit_price" => floatval($_POST['monto']),
+			
+			"category_id"=>"travel"
+		)
+    ),
+	"payer" => array(
+		"name" =>$_POST ['nombre'],
+		"surname"=>$_POST ['apellido'],
+		"email"=>$_POST ['email'],
+		"identification"=> array(
+			
+				"type" =>$_POST ['tipo_dni'],
+				"number" =>$_POST ['nro']
+				
+								)
+		
+		
+					),
+	"external_reference" => $_POST['file'],
+	"expires"=> false,
+	"sponsor_id" => 204442777 
+	
+	
+);
+
+$preference = $mp->create_preference($preference_data);
+
+echo $preference["response"]['init_point'];
+
+?>
+
